98400f261d4bfa07bcf57c9c470108b3d1edfbfb
George Shakhnazaryan <gshakhnazaryan@backstopsolutions.com>
2012-02-09 00:15:32 -0600

    Added ability to run the tests in headless mode and record failures.

--- Gemfile
+++ Gemfile
@@ -1,4 +1,4 @@
 source "http://rubygems.org"
 gem "rspec", "> 2"
 gem "selenium-webdriver"
-
+gem "headless"
--- lib/rspec/spec_helper.rb
+++ lib/rspec/spec_helper.rb
@@ -2,14 +2,26 @@ require 'rubygems'
 require 'rspec/core'
 require 'base64'
 require 'fileutils'
+require 'headless'
 require File.expand_path(File.dirname(__FILE__) + "/rspec_extensions")
 require File.expand_path(File.dirname(__FILE__) + "/reporting/selenium_test_report_formatter")
 
 RSpec.configure do |config|
 
+  config.before(:all) do
+    @headless = Headless.new
+    @headless.start
+  end
+
+  config.before(:each) do
+    @headless.video.start_capture
+  end
+
   config.after(:each) do 
     if actual_failure? 
-      SeleniumTestReportFormatter.capture_system_state(@driver, self.example)
+      SeleniumTestReportFormatter.capture_system_state(@driver, @headless, self.example)
+    else
+      @headless.video.stop_and_discard
     end
   end
 
